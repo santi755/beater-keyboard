89057860f95f1f76219832b1bdd1f0414d1d086f
Santiago <santi755@gmail.com>
2024-02-22 15:59:18 +0100

    Add guitar play button to Sequalizer page

--- src/app/sequalizer/page.tsx
+++ src/app/sequalizer/page.tsx
@@ -1,9 +1,16 @@
 import { NoteButton } from "@/components/keyboard/atoms/NoteButton";
 
+import Guitar from "@/core/domain/Guitar";
+
+const guitar = Guitar.create("Guitar");
+
+// https://nextjs.org/docs/pages/building-your-application/rendering/client-side-rendering
+// Try to render the page on the client side
 export default function Sequalizer() {
   return (
     <main>
       <h1 className="text-3xl font-bold">Sequalizer!</h1>
+      <button onClick={() => guitar.play("C4", "8")}>Play C4</button>
       <NoteButton />
     </main>
   );
