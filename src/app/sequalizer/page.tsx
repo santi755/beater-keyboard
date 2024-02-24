f5cc9b1cc585ef58c6fed92c7bcbeee34bf0340f
Santiago Mansilla Labbé <santi755@gmail.com>
2024-02-24 20:37:44 +0100

    Refactor Sequalizer component to use GuitarService

--- src/app/sequalizer/page.tsx
+++ src/app/sequalizer/page.tsx
@@ -4,26 +4,25 @@ import { useEffect, useState } from "react";
 
 import { NoteButton } from "@/components/keyboard/atoms/NoteButton";
 
-import Guitar from "@/core/domain/Guitar";
 import GuitarService from "@/core/application/GuitarService";
 import ToneSoundPlayer from "@/core/infrastructure/ToneSoundPlayer";
 
 export default function Sequalizer() {
+  const [guitar, setGuitar] = useState<GuitarService | null>(null);
 
   useEffect(() => {
-    const guitarInstance = Guitar.create("Acoustic");
-    setGuitar(guitarInstance);
+    const toneSoundPlayer = new ToneSoundPlayer();
+    const guitarService = new GuitarService(toneSoundPlayer);
 
-  const toneSoundPlayer = new ToneSoundPlayer();
-  const guitarService = new GuitarService(toneSoundPlayer);
+    setGuitar(guitarService);
   }, []);
 
   return (
     <main>
       <h1 className="text-3xl font-bold">Sequalizer!</h1>
       <button
-        onMouseDown={() => guitarService.playSound("C4", "8")}
-        onMouseUp={() => guitarService.stopSound("C4")}
+        onMouseDown={() => guitar?.playSound("C4", "8")}
+        onMouseUp={() => guitar?.stopSound("C4")}
         >Play C4</button>
       <NoteButton />
     </main>
