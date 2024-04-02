963437e20e2249b99702b492360bd122b644c0f1
Santiago Mansilla Labbé <santi755@gmail.com>
2024-04-02 22:44:06 +0200

    Refactor board initialization and grid building

--- src/core/contexts/board/application/BoardService.ts
+++ src/core/contexts/board/application/BoardService.ts
@@ -2,14 +2,11 @@ import Board from '@/core/contexts/board/domain/Board';
 import GridBuilder from '@/core/contexts/board/domain/GridBuilder';
 import BoardDimension from '../domain/BoardDimension';
 
-const CELL_WIDTH = 40;
-const CELL_HEIGHT = 32;
-
 export default class StepboardService {
   constructor(private gridBuilder: GridBuilder) {}
 
   public initializeBoard(): Board {
-    return Board.create([], false, 0, new BoardDimension(16, 8));
+    return Board.create([], false, 0, new BoardDimension(800, 384));
   }
 
   public buildGrid(board: Board): void {
--- src/core/contexts/board/infrastructure/CanvasGridBuilder.ts
+++ src/core/contexts/board/infrastructure/CanvasGridBuilder.ts
@@ -1,6 +1,9 @@
 import GridBuilder from '@/core/contexts/board/domain/GridBuilder';
 import Board from '@/core/contexts/board/domain/Board';
 
+const CELL_WIDTH = 40;
+const CELL_HEIGHT = 32;
+
 export default class CanvasGridBuilder implements GridBuilder {
   public constructor(
     private readonly canvas: HTMLCanvasElement = document.createElement(
@@ -10,8 +13,6 @@ export default class CanvasGridBuilder implements GridBuilder {
 
   build(board: Board): void {
     const context = this.canvas.getContext('2d');
-    const ACTIVE_CELL_COLOR = '#1abc9c';
-    const INACTIVE_CELL_COLOR = '#ffffff';
     const BORDER_COLOR = '#333';
 
     if (!context) {
@@ -19,42 +20,19 @@ export default class CanvasGridBuilder implements GridBuilder {
       throw new Error('Canvas context not found');
     }
 
-    console.log('board => ', board);
-    /*
-    if (!grid) {
-      // TODO: Use a custom error
-      throw new Error('Grid not found');
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    context.strokeStyle = BORDER_COLOR;
+
+    for (let x = 0; x <= this.canvas.width; x += CELL_WIDTH) {
+      context.moveTo(x, 0);
+      context.lineTo(x, this.canvas.height);
     }
 
-    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (let y = 0; y <= this.canvas.height; y += CELL_HEIGHT) {
+      context.moveTo(0, y);
+      context.lineTo(this.canvas.width, y);
+    }
 
-    const gridMatrix = Array.from(grid.matrix.values());
-
-    // TODO: refactor to use design pattern and avoid nested loops and if statements
-    gridMatrix.forEach((steps, stepPositionX) => {
-      steps.forEach((step, stepPositionY) => {
-        if (step.duration) {
-          context.fillStyle = ACTIVE_CELL_COLOR;
-        } else {
-          context.fillStyle = INACTIVE_CELL_COLOR;
-        }
-
-        context.fillRect(
-          stepPositionY * grid.cellWidth,
-          stepPositionX * grid.cellHeight,
-          grid.cellWidth,
-          grid.cellHeight
-        );
-
-        context.strokeStyle = BORDER_COLOR;
-        context.strokeRect(
-          stepPositionY * grid.cellWidth,
-          stepPositionX * grid.cellHeight,
-          grid.cellWidth,
-          grid.cellHeight
-        );
-      });
-    });
-    */
+    context.stroke();
   }
 }
