b109929dd78178b582d02c76bcf2dde38e337f03
Santiago <santi755@gmail.com>
2024-05-15 13:36:01 +0200

    Add InstrumentRepositoryMock for unit testing

---
+++ core/__tests__/unit/instrument/domain/InstrumentRepositoryMock.ts
@@ -0,0 +1,40 @@
+import { expect, vi } from 'vitest';
+
+import InstrumentRepository from '@core/contexts/instrument/domain/InstrumentRepository';
+import Instrument from '@core/contexts/instrument/domain/Instrument';
+
+export default class InstrumentRepositoryMock implements InstrumentRepository {
+  private saveMock = vi.fn();
+  private saveAllMock = vi.fn();
+  private findAllMock = vi.fn();
+  private getSelectedInstrumentMock = vi.fn();
+  private selectInstrumentMock = vi.fn();
+
+  save(instrument: Instrument): Promise<void> {
+    return this.saveMock(instrument);
+  }
+
+  saveAll(instruments: Instrument[]): Promise<void> {
+    return this.saveAllMock(instruments);
+  }
+
+  findAll(): Promise<Instrument[]> {
+    return this.findAllMock();
+  }
+
+  getSelectedInstrument(): Promise<Instrument | null> {
+    return this.getSelectedInstrumentMock();
+  }
+
+  selectInstrument(instrument: Instrument): void {
+    this.selectInstrumentMock(instrument);
+  }
+
+  shouldSave(): void {
+    expect(this.saveMock).toHaveBeenCalled();
+  }
+
+  shouldSaveAll(): void {
+    expect(this.saveAllMock).toHaveBeenCalled();
+  }
+}
