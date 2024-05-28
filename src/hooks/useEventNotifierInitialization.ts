6e14442037d45e3a04cf711e4728510f3877d5b3
Santiago <santi755@gmail.com>
2024-05-28 20:41:54 +0200

    feat: Add DrawNoteOnGrid to board configuration

--- src/hooks/useEventNotifierInitialization.ts
+++ src/hooks/useEventNotifierInitialization.ts
@@ -1,3 +1,4 @@
+import DrawNoteOnGrid from '@core/contexts/board/application/DrawNoteOnGrid';
 import { TYPES } from '@core/config/types';
 import GridDrawer from '@core/contexts/board/domain/GridDrawer';
 import EventTypes from '@core/contexts/shared/domain/events/EventTypes';
@@ -9,15 +10,15 @@ import { useEffect } from 'react';
 
 function useEventNotifierInitialization(container: Container) {
   useEffect(() => {
-    const gridDrawer = container.get<GridDrawer & Observer<ClickEvent>>(
-      TYPES.GridDrawer
+    const drawNoteOnGrid = container.get<DrawNoteOnGrid & Observer<ClickEvent>>(
+      TYPES.DrawNoteOnGrid
     );
 
     const eventNotifier = container.get<EventNotifier<EventTypes>>(
       TYPES.EventNotifier
     );
 
-    eventNotifier.subscribe('CLICK_EVENT', gridDrawer);
+    eventNotifier.subscribe('CLICK_EVENT', drawNoteOnGrid);
   }, [container]);
 }
 
